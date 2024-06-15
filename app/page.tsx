7e337b046ba1b84ef9bdd8c3f8ea1d0a71a4212a
mkeloo <mkeloo@ufl.edu>
2024-06-14 22:47:36 -0400

    finished RecentProjects, Clients, Experience, and Approach components to Home page. Onto Footer now.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import Hero from '@/components/Hero';
 import { FloatingNav } from '@/components/ui/FloatingNav';
 import Grid from '@/components/Grid';
-import { FaHome } from 'react-icons/fa';
+import RecentProjects from '@/components/RecentProjects';
+import Clients from '@/components/Clients';
+import Experience from '@/components/Experience';
+import Approach from '@/components/Approach';
+import { navItems } from '@/data';
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="mx-w-7xl w-full">
-        <FloatingNav
-          navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
       </div>
     </main>
   );
