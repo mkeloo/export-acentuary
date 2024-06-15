2074dd42a66e6033150eff40895d6f2708814911
mkeloo <mkeloo@ufl.edu>
2024-06-14 23:10:40 -0400

    Project finished.

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,12 @@ import RecentProjects from '@/components/RecentProjects';
 import Clients from '@/components/Clients';
 import Experience from '@/components/Experience';
 import Approach from '@/components/Approach';
+import Footer from '@/components/Footer';
 import { navItems } from '@/data';
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="mx-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -18,6 +19,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
